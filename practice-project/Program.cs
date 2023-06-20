c5bde420cde7e244af8a6073a022672c943e0b6b
Adeeshjn <asij12317@gmail.com>
2023-06-20 12:10:06 +0530

    Assignment 3 done all

--- practice-project/Program.cs
+++ practice-project/Program.cs
@@ -1,20 +1,20 @@
 ﻿// write a program to check how many times 3 appears in a number
-using System.CodeDom.Compiler;
+using System.ComponentModel;
 
 void checkDigit()
 {
     Console.WriteLine("Enter the number: ");
     int n = Convert.ToInt32(Console.ReadLine());
     int count = 0;
-    while (n > 0)
+    while(n > 0)
     {
-        if (n % 10 == 3)
+        if(n % 10 == 3)
         {
             count++;
-            n /= 10;
+            n/=10;
         }
-        else
-            n /= 10;
+       else
+            n/=10;
 
     }
     Console.WriteLine($"The number of times 3 appears in the number is {count}");
@@ -26,11 +26,11 @@ void CheckPalindrome()
     Console.WriteLine("Enter the number: ");
     string n = Console.ReadLine();
     int len = n.Length;
-    for (int i = 0; i <= len - 1; i++)
+    for( int i = 0; i <= len-1; i++ )
     {
-        if (n[i] != n[len - i - 1])
+        if (n[i] != n[len-i-1] )
         {
-            Console.WriteLine("Not a palindrome");
+            Console.WriteLine("Not a palindrome"); 
             break;
         }
     }
@@ -46,9 +46,9 @@ void fibonacciSeries()
     int num = Convert.ToInt32(Console.ReadLine());
     Console.WriteLine("The terms of the fibonacci Series are:");
     Console.Write($"{a}, {b}");
-    for (int i = 0; i < num - 2; i++)
+    for(int i =0; i < num-2; i++ )
     {
-        c = b;
+        c=b;
         b = a + b;
         a = c;
         Console.Write($", {b}");
@@ -56,70 +56,6 @@ void fibonacciSeries()
 }
 
 // Given two dates - first from the past and second as present date, write a method that returns numbers of days between these two dates.
-int findDiff(DateTime d1, DateTime d2)
-{
-    int days = 0;
-    int[] noofdaysinmonth = { 31,28,31,30,31,30,31,31,30,31,30,31};
-    if(d1 <= d2)
-    {
-        if (d1.Year == d2.Year)
-        {
-            if ((d1.Month == d2.Month) && (d1.Day == d2.Day))
-            {
-                return 0;
-            }
-            if (d1.Month == d2.Month)
-            {
-                return d2.Day - d1.Day;
-            }
-            for (int i = d1.Month; i < d2.Month - 1; i++)
-            {
-                days += noofdaysinmonth[i];
-            }
-            days += noofdaysinmonth[d1.Month - 1] - d1.Day + d2.Day;
-        }
-        else
-        {
-            for (int i = d1.Month; i < d2.Month - 1; i++)
-            {
-                days += noofdaysinmonth[i];
-                Console.WriteLine("Hi");
-            }
-            days += (d2.Year - d1.Year) * 365 + (d2.Year - d1.Year) / 4;
-            if (d1.Month == d2.Month)
-                days += d2.Day - d1.Day;
-            else
-            {
-                days += noofdaysinmonth[d1.Month - 1] - d1.Day + d2.Day;
-            }
-        }
-        
-        return days;
-    }
-    else
-        return findDiff(d2, d1);
-}
-int days(DateTime d)
-{
-    int days = 0;
-    int[] noofdaysinmonth = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-    days= d.Year*365;
-    days += d.Year / 4 - d.Year / 400 + d.Year / 100;
-    for (int i = 0; i < d.Month - 1; i++)
-    {
-        days += noofdaysinmonth[i];
-    }
-
-    if (d.Month == 2 && d.Day<29 )
-    {
-        days--;
-    }
-    if (d.Month == 1)
-        days--;
-
-    days += d.Day;
-    return days;
-}
 void Difference()
 {
     Console.WriteLine("Enter the first date:");
@@ -127,22 +63,39 @@ void Difference()
     string date = dateTime1.ToString();
     Console.WriteLine("Enter the Second date:");
     DateTime dateTime2 = Convert.ToDateTime(Console.ReadLine());
-    
-    TimeSpan diff = dateTime1 - dateTime2;
-    if (diff.Days<0)
-    {
-        diff = dateTime2 - dateTime1;
-    }
-    int noofdays=findDiff(dateTime1,dateTime2);
-    int diffman = days(dateTime1) - days(dateTime2);
-    if(diffman < 0)
-    {
-        diffman = -diffman;
-    }
-    Console.WriteLine($"The difference in between the dates is:{diff.Days}, {noofdays}, {diffman}");
+    TimeSpan diff  = dateTime1 - dateTime2;
+    Console.WriteLine($"The difference in between the dates is:{diff.Days}");
 }
 
 Difference();
+// write a method that swaps them using temporary variable and without temp variable.
+void SwapWithTemp()
+{
+    Console.WriteLine("Enter the first date:");
+    DateTime dateTime1 = Convert.ToDateTime(Console.ReadLine());
+    string date = dateTime1.ToString();
+    Console.WriteLine("Enter the Second date:");
+    DateTime dateTime2 = Convert.ToDateTime(Console.ReadLine());
+    DateTime temp = dateTime1;
+    dateTime1 = dateTime2;
+    dateTime2 = temp;
+    Console.WriteLine($"The first date is {dateTime1} and the second date is {dateTime2}");
+}
+
+// Swap without temp variable
+void SwapWithoutTemp()
+{
+    Console.WriteLine("Enter the first date:");
+    DateTime dateTime1 = Convert.ToDateTime(Console.ReadLine());
+    string date = dateTime1.ToString();
+    Console.WriteLine("Enter the Second date:");
+    DateTime dateTime2 = Convert.ToDateTime(Console.ReadLine());
+    TimeSpan timeSpan = dateTime1 - dateTime2;
+    dateTime1 = dateTime1.Add(timeSpan);
+    dateTime2 = dateTime2.Subtract(timeSpan);
+    
+}   
+
 /*
  * Write a C# program to find the eligibility of admission for a professional course based on the following criteria: 
  * Marks in Maths >=65
@@ -172,12 +125,12 @@ void CheckEligiblity()
 // Write a method that prints 10 by 10 multiplication table. Remember about readibility (spaces in the right place).
 void MultiplicationTable()
 {
-    for (int i = 1; i <= 10; i++)
+    for(int i = 1; i <= 10; i++)
     {
-        for (int j = 1; j <= 10; j++)
+        for(int j = 1; j <= 10; j++)
         {
             Console.Write($"{i * j} ");
-            if (i * j < 10)
+            if (i*j<10)
             {
                 Console.Write(" ");
             }
@@ -185,4 +138,3 @@ void MultiplicationTable()
         Console.WriteLine();
     }
 }
-
